fd87822c56714b2370e1cb5d21d7ca017ed9bb9e
Mix-Liten <mixliten@gmail.com>
2022-01-18 12:09:11 +0800

    Remix_Crash_Course

---
+++ experiment/Remix_Crash_Course/app/routes/posts/index.jsx
@@ -0,0 +1,45 @@
+import { Link, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+import { getUser } from "~/utils/session.server";
+
+export const loader = async ({ request }) => {
+  const data = {
+    user: await getUser(request),
+    posts: await db.post.findMany({
+      take: 20,
+      select: { id: true, title: true, createdAt: true },
+      orderBy: { createdAt: "desc" },
+    }),
+  };
+
+  return data;
+};
+
+function PostItems() {
+  const { user, posts } = useLoaderData();
+  
+  return (
+    <>
+      <div className="page-header">
+        <h1>Posts</h1>
+        {user && (
+          <Link to="/posts/new" className="btn">
+            New Post
+          </Link>
+        )}
+      </div>
+      <ul className="posts-list">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.id}>
+              <h3>{post.title}</h3>
+              {new Date(post.createdAt).toLocaleString()}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default PostItems;
