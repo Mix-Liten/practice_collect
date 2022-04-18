e54e897b8259c442cb5d587b50fbf5f35c0da787
Mix-Liten <mixliten@gmail.com>
2022-04-18 17:45:05 +0800

    ui/Dropdown_User_Menu

---
+++ ui/Dropdown_User_Menu/app.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.menuToggle')
+const navigation = document.querySelector('.navigation')
+
+menuToggle.onclick = function () {
+  navigation.classList.toggle('active')
+}
+
