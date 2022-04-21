a3e3c299449b41a6cb371a9a0968ed0a08658ca3
Mix-Liten <mixliten@gmail.com>
2022-04-22 02:32:07 +0800

    experiment/hastebin_clone

---
+++ experiment/hastebin_clone/controllers/documentController.js
@@ -0,0 +1,55 @@
+const Document = require('../models/Document')
+
+const index = (req, res) => {
+  const variables = {
+    title: 'Hastebin',
+    language: 'plaintext',
+    code: `Welcome to HasteBin! \n
+Use the commands in the top right corner
+to create a new file to share with others.
+  `,
+  }
+  res.render('show', variables)
+}
+
+const create = async (req, res) => {
+  const id = req.params.id
+  if (!id) {
+    res.render('create')
+    return
+  }
+
+  try {
+    const document = await Document.findById(id)
+    res.render('create', { content: document.content })
+  } catch (err) {
+    res.redirect('/')
+  }
+}
+
+const store = async (req, res) => {
+  const content = req.body.content
+  try {
+    const document = await Document.create({ content })
+    res.redirect(`/show/${document.id}`)
+  } catch (err) {
+    res.render('create', { content })
+  }
+}
+
+const show = async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+    res.render('show', { code: document.content, id })
+  } catch (err) {
+    res.redirect('/')
+  }
+}
+
+module.exports = {
+  index,
+  create,
+  store,
+  show,
+}
---
+++ experiment/hastebin_clone/routes/index.js
@@ -0,0 +1,12 @@
+const router =  require('express').Router()
+const documentController = require('../controllers/documentController')
+
+router.get('/', documentController.index)
+
+router.get('/create/:id?', documentController.create)
+
+router.post('/store', documentController.store)
+
+router.get('/show/:id', documentController.show)
+
+module.exports = router
