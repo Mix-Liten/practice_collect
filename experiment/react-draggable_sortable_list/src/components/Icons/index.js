d08d8c6cb3c22e08ad4d3705ef50424e26319f0a
Mix-Liten <mixliten@gmail.com>
2022-05-06 20:09:14 +0800

    experiment/react-draggable_sortable_list

---
+++ experiment/react-draggable_sortable_list/src/components/Icons/index.js
@@ -0,0 +1,13 @@
+import TruthyIcon from './CheckCircle'
+import FalslyIcon from './XCircle'
+import MenuIcon from './Menu'
+import SaveIcon from './Save'
+
+const Icons = {
+  TruthyIcon,
+  FalslyIcon,
+  MenuIcon,
+  SaveIcon,
+}
+
+export { Icons as default, TruthyIcon, FalslyIcon, MenuIcon, SaveIcon }
